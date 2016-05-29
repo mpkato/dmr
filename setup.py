15559ff7142fafd634c43ebb0fd55dbad73c90c7
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-05-29 11:58:32 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = "dmr",
+    packages = ["dmr"],
+    version = "0.0.1",
+    description = "Topic Models with Dirichlet-multinomial Regression",
+    author = "Makoto P. Kato",
+    author_email = "kato@dl.kuis.kyoto-u.ac.jp",
+    license     = "MIT License",
+    url = "https://github.com/mpkato/dmr",
+    install_requires = [
+        'numpy'
+    ],
+    tests_require=['nose']
+)
